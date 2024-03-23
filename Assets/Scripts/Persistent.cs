5dc61a537ed57ce205f0094418950b8bbf4bce05
np31-Nik <nik.trabajos@gmail.com>
2024-03-23 04:17:28 +0100

    Menu upgrades, UI FOG, persistent singleton for music

--- Assets/Scripts/Persistent.cs
+++ Assets/Scripts/Persistent.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Persistent : MonoBehaviour
 {
     private AudioSource _audioSource;
+    static Persistent instance;
     
     private void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
-        _audioSource = GetComponent<AudioSource>();
+        if(instance != null){
+            Destroy(gameObject);
+        }else{
+            instance = this;
+            DontDestroyOnLoad(transform.gameObject);
+            _audioSource = GetComponent<AudioSource>();
+        }
+
     }
 
     public void PlayMusic()
